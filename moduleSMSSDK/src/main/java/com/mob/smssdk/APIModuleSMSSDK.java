9ac1994bbe565acdaf31c0a986a125a2f6dd5aaf
Jackie Wayne <jackiewayne2046@163.com>
2019-01-30 17:40:27 +0800

    与iOS统一命名

--- moduleSMSSDK/src/main/java/com/mob/smssdk/APIModuleSMSSDK.java
+++ moduleSMSSDK/src/main/java/com/mob/smssdk/APIModuleSMSSDK.java
@@ -9,12 +9,16 @@ import com.uzmap.pkg.uzcore.uzmodule.UZModuleContext;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import cn.smssdk.EventHandler;
 import cn.smssdk.SMSSDK;
 
 public class APIModuleSMSSDK extends UZModule {
+	private static final String KEY_CODE = "code";
+	private static final String KEY_MSG = "msg";
+	private static final int BRIDGE_ERR = 700;
 	private static final String ERROR_INTERNAL = "APICloud bridge internal error: ";
 	private Hashon hashon;
 
@@ -44,7 +48,7 @@ public class APIModuleSMSSDK extends UZModule {
 //									res.put("smart", smart);
 //									throwSuccess(moduleContext, res);
 //								} catch (JSONException e) {
-//									SMSSDKLog.e("jsmethod_getVerificationCode() internal error. msg= " + e.getMessage(), e);
+//									SMSSDKLog.e("jsmethod_getCode() internal error. msg= " + e.getMessage(), e);
 //									throwInternalError(moduleContext, "Generate JSONObject error");
 //								}
 //							}
@@ -58,12 +62,12 @@ public class APIModuleSMSSDK extends UZModule {
 //										JSONObject res = new JSONObject(msg);
 //										throwSdkError(moduleContext, res);
 //									} catch (JSONException e) {
-//										SMSSDKLog.e("jsmethod_getVerificationCode() internal error. msg= " + e.getMessage(), e);
+//										SMSSDKLog.e("jsmethod_getCode() internal error. msg= " + e.getMessage(), e);
 //										throwInternalError(moduleContext,"Generate JSONObject error");
 //									}
 //								} else {
 //									String msg = "Sdk returned 'RESULT_ERROR', but the data is NOT an instance of Throwable";
-//									SMSSDKLog.e("jsmethod_getVerificationCode() internal error: " + msg);
+//									SMSSDKLog.e("jsmethod_getCode() internal error: " + msg);
 //									throwInternalError(moduleContext, msg);
 //								}
 //							}
@@ -75,8 +79,8 @@ public class APIModuleSMSSDK extends UZModule {
 //		SMSSDK.registerEventHandler(callback);
 //	}
 
-	public void jsmethod_getVerificationCode(final UZModuleContext moduleContext){
-		SMSSDKLog.d("jsmethod_getVerificationCode()");
+	public void jsmethod_getTextCode(final UZModuleContext moduleContext){
+		SMSSDKLog.d("jsmethod_getTextCode()");
 		// 注册监听器
 		EventHandler callback = new EventHandler() {
 			@Override
@@ -93,7 +97,7 @@ public class APIModuleSMSSDK extends UZModule {
 									res.put("smart", smart);
 									throwSuccess(moduleContext, res);
 								} catch (JSONException e) {
-									SMSSDKLog.e("jsmethod_getVerificationCode() internal error. msg= " + e.getMessage(), e);
+									SMSSDKLog.e("jsmethod_getTextCode() internal error. msg= " + e.getMessage(), e);
 									throwInternalError(moduleContext, "Generate JSONObject error");
 								}
 							}
@@ -103,16 +107,10 @@ public class APIModuleSMSSDK extends UZModule {
 								if (data instanceof Throwable) {
 									Throwable throwable = (Throwable) data;
 									String msg = throwable.getMessage();
-									try {
-										JSONObject res = new JSONObject(msg);
-										throwSdkError(moduleContext, res);
-									} catch (JSONException e) {
-										SMSSDKLog.e("jsmethod_getVerificationCode() internal error. msg= " + e.getMessage(), e);
-										throwInternalError(moduleContext,"Generate JSONObject error");
-									}
+									throwSdkError(moduleContext, msg);
 								} else {
 									String msg = "Sdk returned 'RESULT_ERROR', but the data is NOT an instance of Throwable";
-									SMSSDKLog.e("jsmethod_getVerificationCode() internal error: " + msg);
+									SMSSDKLog.e("jsmethod_getTextCode() internal error: " + msg);
 									throwInternalError(moduleContext, msg);
 								}
 							}
@@ -125,17 +123,17 @@ public class APIModuleSMSSDK extends UZModule {
 
 		// 调用接口
 		String tempCode = moduleContext.optString("tempCode");
-		String country = moduleContext.optString("country");
-		String phone = moduleContext.optString("phone");
+		String zone = moduleContext.optString("zone");
+		String phoneNumber = moduleContext.optString("phoneNumber");
 
 		SMSSDKLog.d("tempCode: " + tempCode);
-		SMSSDKLog.d("country: " + country);
-		SMSSDKLog.d("phone: " + phone);
-		SMSSDK.getVerificationCode(tempCode, country, phone);
+		SMSSDKLog.d("zone: " + zone);
+		SMSSDKLog.d("phoneNumber: " + phoneNumber);
+		SMSSDK.getVerificationCode(tempCode, zone, phoneNumber);
 	}
 
-	public void jsmethod_getVoiceVerifyCode(final UZModuleContext moduleContext){
-		SMSSDKLog.d("jsmethod_getVoiceVerifyCode()");
+	public void jsmethod_getVoiceCode(final UZModuleContext moduleContext){
+		SMSSDKLog.d("jsmethod_getVoiceCode()");
 		// 注册监听器
 		EventHandler callback = new EventHandler() {
 			@Override
@@ -156,16 +154,10 @@ public class APIModuleSMSSDK extends UZModule {
 								if (data instanceof Throwable) {
 									Throwable throwable = (Throwable) data;
 									String msg = throwable.getMessage();
-									try {
-										JSONObject res = new JSONObject(msg);
-										throwSdkError(moduleContext, res);
-									} catch (JSONException e) {
-										SMSSDKLog.e("jsmethod_getVoiceVerifyCode() internal error. msg= " + e.getMessage(), e);
-										throwInternalError(moduleContext,"Generate JSONObject error");
-									}
+									throwSdkError(moduleContext, msg);
 								} else {
 									String msg = "Sdk returned 'RESULT_ERROR', but the data is NOT an instance of Throwable";
-									SMSSDKLog.e("jsmethod_getVoiceVerifyCode() internal error: " + msg);
+									SMSSDKLog.e("jsmethod_getVoiceCode() internal error: " + msg);
 									throwInternalError(moduleContext, msg);
 								}
 							}
@@ -177,16 +169,16 @@ public class APIModuleSMSSDK extends UZModule {
 		SMSSDK.registerEventHandler(callback);
 
 		// 调用接口
-		String country = moduleContext.optString("country");
-		String phone = moduleContext.optString("phone");
+		String zone = moduleContext.optString("zone");
+		String phoneNumber = moduleContext.optString("phoneNumber");
 
-		SMSSDKLog.d("country: " + country);
-		SMSSDKLog.d("phone: " + phone);
-		SMSSDK.getVoiceVerifyCode(country, phone);
+		SMSSDKLog.d("zone: " + zone);
+		SMSSDKLog.d("phoneNumber: " + phoneNumber);
+		SMSSDK.getVoiceVerifyCode(zone, phoneNumber);
 	}
 
-	public void jsmethod_submitVerificationCode(final UZModuleContext moduleContext){
-		SMSSDKLog.d("jsmethod_submitVerificationCode()");
+	public void jsmethod_commitCode(final UZModuleContext moduleContext){
+		SMSSDKLog.d("jsmethod_commitCode()");
 		// 注册监听器
 		EventHandler callback = new EventHandler() {
 			@Override
@@ -198,18 +190,19 @@ public class APIModuleSMSSDK extends UZModule {
 							if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
 								// callback onSuccess
 								// data示例：{country=86, phone=13362206853}
-								HashMap<String, Object> map = (HashMap<String, Object>)data;
-								JSONObject res = new JSONObject();
-								try {
-									if (map != null && !map.isEmpty()) {
-										res.put("country", map.get("country"));
-										res.put("phone", map.get("phone"));
-									}
-									throwSuccess(moduleContext, res);
-								} catch (JSONException e) {
-									SMSSDKLog.e("jsmethod_submitVerificationCode() internal error. msg= " + e.getMessage(), e);
-									throwInternalError(moduleContext, "Generate JSONObject error");
-								}
+//								HashMap<String, Object> map = (HashMap<String, Object>)data;
+//								JSONObject res = new JSONObject();
+//								try {
+//									if (map != null && !map.isEmpty()) {
+//										res.put("country", map.get("country"));
+//										res.put("phone", map.get("phone"));
+//									}
+//									throwSuccess(moduleContext, res);
+//								} catch (JSONException e) {
+//									SMSSDKLog.e("jsmethod_commitCode() internal error. msg= " + e.getMessage(), e);
+//									throwInternalError(moduleContext, "Generate JSONObject error");
+//								}
+								throwSuccess(moduleContext, null);
 							}
 						} else {
 							if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
@@ -217,16 +210,10 @@ public class APIModuleSMSSDK extends UZModule {
 								if (data instanceof Throwable) {
 									Throwable throwable = (Throwable) data;
 									String msg = throwable.getMessage();
-									try {
-										JSONObject res = new JSONObject(msg);
-										throwSdkError(moduleContext, res);
-									} catch (JSONException e) {
-										SMSSDKLog.e("jsmethod_submitVerificationCode() internal error. msg= " + e.getMessage(), e);
-										throwInternalError(moduleContext,"Generate JSONObject error");
-									}
+									throwSdkError(moduleContext, msg);
 								} else {
 									String msg = "Sdk returned 'RESULT_ERROR', but the data is NOT an instance of Throwable";
-									SMSSDKLog.e("jsmethod_submitVerificationCode() internal error: " + msg);
+									SMSSDKLog.e("jsmethod_commitCode() internal error: " + msg);
 									throwInternalError(moduleContext, msg);
 								}
 							}
@@ -238,28 +225,84 @@ public class APIModuleSMSSDK extends UZModule {
 		SMSSDK.registerEventHandler(callback);
 
 		// 调用接口
-		String country = moduleContext.optString("country");
-		String phone = moduleContext.optString("phone");
+		String zone = moduleContext.optString("zone");
+		String phoneNumber = moduleContext.optString("phoneNumber");
 		String code = moduleContext.optString("code");
 
-		SMSSDKLog.d("country: " + country);
-		SMSSDKLog.d("phone: " + phone);
+		SMSSDKLog.d("zone: " + zone);
+		SMSSDKLog.d("phoneNumber: " + phoneNumber);
 		SMSSDKLog.d("code: " + code);
-		SMSSDK.submitVerificationCode(country, phone, code);
+		SMSSDK.submitVerificationCode(zone, phoneNumber, code);
+	}
+
+	public void jsmethod_getSupportedCountries(final UZModuleContext moduleContext){
+		SMSSDKLog.d("jsmethod_getSupportedCountries()");
+		// 注册监听器
+		EventHandler callback = new EventHandler() {
+			@Override
+			public void afterEvent(final int event, final int result, final Object data) {
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						if (result == SMSSDK.RESULT_COMPLETE) {
+							if (event == SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES) {
+								// callback onSuccess
+								// data示例：[{zone=590, rule=^\d+}, {zone=680, rule=^\d+}]
+								ArrayList<HashMap<String, Object>> list = (ArrayList<HashMap<String, Object>>)data;
+								HashMap<String, Object> map = new HashMap<String, Object>();
+								map.put("countries", list);
+								JSONObject res = new JSONObject(map);
+								throwSuccess(moduleContext, res);
+							}
+						} else {
+							if (event == SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES) {
+								// callback onError
+								if (data instanceof Throwable) {
+									Throwable throwable = (Throwable) data;
+									String msg = throwable.getMessage();
+									throwSdkError(moduleContext, msg);
+								} else {
+									String msg = "Sdk returned 'RESULT_ERROR', but the data is NOT an instance of Throwable";
+									SMSSDKLog.e("jsmethod_getSupportedCountries() internal error: " + msg);
+									throwInternalError(moduleContext, msg);
+								}
+							}
+						}
+					}
+				});
+			}
+		};
+		SMSSDK.registerEventHandler(callback);
+
+		// 调用接口
+		SMSSDK.getSupportedCountries();
 	}
 
 	private void throwSuccess(UZModuleContext moduleContext, JSONObject res) {
 		moduleContext.success(res, true);
 	}
 
-	private void throwSdkError(UZModuleContext moduleContext, JSONObject error) {
-		moduleContext.error(null, error, true);
+	private void throwSdkError(UZModuleContext moduleContext, String error) {
+		try {
+			JSONObject resp = new JSONObject(error);
+			int code = resp.optInt("status");
+			String msg = resp.optString("error");
+			resp.remove("status");
+			resp.remove("error");
+			resp.put(KEY_CODE, code);
+			resp.put(KEY_MSG, msg);
+			moduleContext.error(null, resp, true);
+		} catch (JSONException e) {
+			SMSSDKLog.e("jsmethod_getSupportedCountries() internal error. msg= " + e.getMessage(), e);
+			throwInternalError(moduleContext,"Generate JSONObject error");
+		}
 	}
 
 	private void throwInternalError(UZModuleContext moduleContext, String errMsg) {
 		JSONObject errJSON = new JSONObject();
 		try {
-			errJSON.put("error", ERROR_INTERNAL + errMsg);
+			errJSON.put(KEY_CODE, BRIDGE_ERR);
+			errJSON.put(KEY_MSG, ERROR_INTERNAL + errMsg);
 		} catch (JSONException e) {
 			SMSSDKLog.e("throwInternalError() encountered exception. msg= " + e.getMessage(), e);
 		}
